67881bdf85b6fc31326457b38de1ac8e29a767fe
Anton Sydorenko <an.sydorenko@gmail.com>
2017-02-12 11:12:05 +0200

    Fixed

--- js/tooltips.js
+++ js/tooltips.js
@@ -3,16 +3,19 @@
  */
 (function ($, undefined) {
     $(function(){
-        var labels = document.body.querySelectorAll('label');
+        var labels = document.body.querySelectorAll('input');
         $(labels).on('mouseenter', function() {
-            $(this).next('.tooltip').fadeIn('slow');
+            $(this).closest('label').next('.tooltip').fadeIn('slow');
         });
         $(labels).on('mouseleave', function() {
-            $(this).next('.tooltip').fadeOut('slow');
+            $(this).closest('label').next('.tooltip').fadeOut('slow');
         });
         $('.showAllButton').on('click', function(e) {
-            $('.tooltip').fadeToggle('slow');
+            $('.tooltip').fadeIn('slow');
             e.preventDefault();
+            setTimeout(function () {
+                $('.tooltip').fadeOut('slow');
+            }, 2000)
         })
     });
 })(jQuery);
