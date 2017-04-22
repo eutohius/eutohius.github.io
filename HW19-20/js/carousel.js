5d3ef256590d200bd889104db56f058f018f4b3f
Anton Sydorenko <an.sydorenko@gmail.com>
2017-04-22 23:26:55 +0300

    made an automatic slider

--- HW19-20/js/carousel.js
+++ HW19-20/js/carousel.js
@@ -4,7 +4,10 @@
 (function($, undefined) {
     $(function() {
         var $carousel = $(document.body.querySelector('.carousel')),
-            $bg = $(document.body.querySelector('.carousel_bg'));
+            $bg = $(document.body.querySelector('.carousel_bg')),
+            $firstRadio = $(document.body.querySelector('.carousel_pagination label')),
+            $currentRadio = $firstRadio;
+
         var $pagination = $carousel.find('.carousel_pagination');
         $pagination.on('change', function() {
             $pagination.find('label').each(function() {
@@ -19,8 +22,18 @@
             });
         });
 
-        setInterval(function() {
+        setInterval(nextButton, 5000);
 
-        }, 5000)
+
+        function nextButton() {
+            console.log($currentRadio.is(':last-child'));
+            $currentRadio.prev()
+                .attr('checked', false);
+            if ($currentRadio.is(':last-child')) $currentRadio = $firstRadio;
+            else $currentRadio = $currentRadio.next().next();
+            $currentRadio.prev()
+                .attr('checked', true)
+                .change();
+        }
     });
 })(jQuery);
